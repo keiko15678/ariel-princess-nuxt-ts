c269d89e174fc2fb606ce86aab064729e923be80
Keiko Chuang <keiko@innotech.me>
2021-01-13 16:19:32 +0800

    20210113-added-heroku-deploy-scripts

---
+++ heroku-deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+HEROKU_API_KEY=heroku-api-key
+HEROKU_APP_NAME=heroku-app-name
+HEROKU_APP_TYPE=web
+
+DOCKER_REMOTE_ADDR=docker-remote-address
+DOCKER_FILE_PATH=Dockerfile
+
+$HEROKU_API_KEY heroku container:login
+
+echo ⭐ Logged-in to Heroku...
+
+heroku config:set --app=$HEROKU_APP_NAME HOST=0.0.0.0
+
+./docker-build-push.sh $DOCKER_REMOTE_ADDR $DOCKER_FILE_PATH
+
+echo ⭐ Image pushed to Dockerhub: $DOCKER_REMOTE_ADDR
+
+docker tag $DOCKER_REMOTE_ADDR registry.heroku.com/$HEROKU_APP_NAME/$HEROKU_APP_TYPE
+docker push registry.heroku.com/$HEROKU_APP_NAME/$HEROKU_APP_TYPE
+
+echo ⭐ Image pushed to Heroku: $HEROKU_APP_NAME
+
+DOCKER_IMAGE_ID=$(docker inspect registry.heroku.com/$HEROKU_APP_NAME/$HEROKU_APP_TYPE --format={{.Id}})
+
+curl --netrc -X PATCH https://api.heroku.com/apps/$HEROKU_APP_NAME/formation \
+  -d '{
+  "updates": [
+    {
+      "type": "'$HEROKU_APP_TYPE'",
+      "docker_image": "'$DOCKER_IMAGE_ID'"
+    }
+  ]
+}' \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+  -o heroku-deploy-output.txt
+
+echo 🤘 Deploy Heroku complete.
