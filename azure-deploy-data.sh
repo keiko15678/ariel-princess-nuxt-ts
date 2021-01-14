5908d04f7b884e38e7d10da344fe5360898f255b
Keiko Chuang <keiko@innotech.me>
2021-01-14 10:11:32 +0800

    20210114-edit-azure-blob-deploy-scripts

--- azure-deploy-data.sh
+++ azure-deploy-data.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+AZURE_STORAGE_ACCOUNT_NAME=xxx
+AZURE_STORAGE_ACCOUNT_PASSWORD=yyy
+
 ./azure-blob-upload.sh ./sample-data.json sample-data.json xxx yyy
+
+for file in static/*; do
+  echo "$(basename "$file")"
+  ./azure-blob-upload.sh /"$file" "$file" $AZURE_STORAGE_ACCOUNT_NAME $AZURE_STORAGE_ACCOUNT_PASSWORD
+  echo ⭐ "$file" uploaded.
+done
